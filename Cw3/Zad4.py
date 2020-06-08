f49239708f896be60e69797b89cf8b8c985629ca
FabianPe226 <44002716+FabianPe226@users.noreply.github.com>
2020-06-08 06:18:21 +0200

    .

---
+++ Cw3/Zad4.py
@@ -0,0 +1,14 @@
+def mono(a,b):
+    if a>0:
+        print("funkcja rosnąca")
+
+    if a==0:
+        print("funkcja stala")
+
+    if a<0:
+        print("funkcja malejaca")
+
+    
+    
+
+print(mono(5,6))
